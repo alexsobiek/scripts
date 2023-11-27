3023c383cb5f7456336ab5db62d7357081f9a5e2
Alex S <38387876+alexsobiek@users.noreply.github.com>
2023-11-27 17:15:34 -0600

    Update mdev-symlink.sh

--- mdev/mdev-symlink.sh
+++ mdev/mdev-symlink.sh
@@ -19,24 +19,34 @@
 declare -A DEV_MAP;
 
 # Map of device names to symlinks
-# Format: DEV_MAP["device_name"]="symlink_name"
-# device name is the path, or the start of the path, of the device
-DEV_MAP["/devices/platform/soc/3f980000.usb"]="/dev/zwave"
+# Format: DEV_MAP["idVendor:idProduct:serial"]="symlink_name"
+DEV_MAP["10c4:ea60:0001"]="/dev/zwave"
+DEV_MAP["10c4:ea60:1612a45e6445ed11873bc88f0a86e0b4"]="/dev/zigbee"
 
-
+device="/sys$DEVPATH"
 
 if [ "$ACTION" = "add" ]; then
-    # Create device symlink
-    for device in "${!DEV_MAP[@]}"; do
-        if [[ "$DEVPATH" == "$device"* ]]; then
-            ln -s "/dev/$MDEV" "${DEV_MAP[$device]}"
+    while [[ "$device" != "/sys" ]]; do
+        if [ -e "$device/idVendor" ] && [ -e "$device/idProduct" ] && [ -e "$device/serial" ]; then
+            idVendor=$(cat "$device/idVendor")
+            idProduct=$(cat "$device/idProduct")
+            serial=$(cat "$device/serial")
+            key="$idVendor:$idProduct:$serial"
+            if [[ -n "${DEV_MAP[$key]}" ]]; then
+                echo "Found device $DEVPATH" >> /var/log/mdev.log
+                echo "Creating symlink "/dev/$MDEV" -> ${DEV_MAP[$key]}" >> /var/log/mdev.log
+                ln -s "/dev/$MDEV" "${DEV_MAP[$key]}"
+            fi
+            break
         fi
+        device=$(dirname "$device")
     done
 elif [ "$ACTION" = "remove" ]; then
-    # Remove device symlink
-    for device in "${!DEV_MAP[@]}"; do
-        if [[ "$DEVPATH" == "$device"* ]]; then
-            rm "${DEV_MAP[$device]}"
+    echo "Removed device $DEVPATH" >> /var/log/mdev.log
+    for symlink in /dev/*; do
+        if [ -L "$symlink" ] && [ ! -e "$symlink" ]; then
+            echo "Removing symlink $symlink" >> /var/log/mdev.log
+            rm "$symlink"
         fi
     done
 fi
